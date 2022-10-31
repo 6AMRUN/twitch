368f3a2ce6d71f02f01c10517dd0f746755c272e
6AMRUN <96393286+6AMRUN@users.noreply.github.com>
2022-10-31 10:04:00 -0700

    Update and rename aziakiara.sql to insert.sql
    changed aziakiara.sql to insert.sql and adding krismf.sql to insert.sql
    changed aziakiara.sql to insert.sql and adding holykray.sql to insert.sql

--- insert.sql
+++ insert.sql
@@ -101,3 +101,59 @@ INSERT INTO clips VALUES ('SpikyObedientWrenchTwitchRPG', 1261970795, 2, '00:00:
 INSERT INTO clips VALUES ('GentleSmoggyBaguetteJebaited', 1261970795, 2, '00:00:27', 'Naofai');
 INSERT INTO clips VALUES ('DifferentTalentedMuleDerp', 1260930535, 2, '00:00:29', 'KhailsWxrld');
 INSERT INTO clips VALUES ('TransparentSeductiveOrcaDAESuppy', 1269620662, 1, '00:00:29', 'Hec4fingers');
+
+
+
+-- INSERTING INTO broadcasts -- krismf
+INSERT INTO broadcasts VALUES (1289337440, 49073484, 99, '02:33:06');
+INSERT INTO broadcasts VALUES (1282813535, 49073484, 98, '03:12:22');
+INSERT INTO broadcasts VALUES (1281518341, 49073484, 68, '01:12:31');
+INSERT INTO broadcasts VALUES (1277467565, 49073484, 84, '03:15:58');
+INSERT INTO broadcasts VALUES (1274541864, 49073484, 131, '04:06:11');
+INSERT INTO broadcasts VALUES (1273828853, 49073484, 112, '04:35:44');
+INSERT INTO broadcasts VALUES (1269901503, 49073484, 179, '04:19:32');
+INSERT INTO broadcasts VALUES (1269516351, 49073484, 69, '02:21:20');
+INSERT INTO broadcasts VALUES (1268830338, 49073484, 26, '00:33:42');
+INSERT INTO broadcasts VALUES (1268477246, 49073484, 113, '03:49:30');
+INSERT INTO broadcasts VALUES (1267786471, 49073484, 206, '07:48:11');
+INSERT INTO broadcasts VALUES (1267236117, 49073484, 149, '04:00:09');
+INSERT INTO broadcasts VALUES (1265727370, 49073484, 189, '06:00:27');
+INSERT INTO broadcasts VALUES (1264530049, 49073484, 98, '03:49:50');
+INSERT INTO broadcasts VALUES (1263742931, 49073484, 69, '01:41:34');
+INSERT INTO broadcasts VALUES (1263491966, 49073484, 96, '03:36:35');
+INSERT INTO broadcasts VALUES (1256731832, 49073484, 221, '02:44:06');
+INSERT INTO broadcasts VALUES (1256681060, 49073484, 89, '00:45:14');
+INSERT INTO broadcasts VALUES (1254580659, 49073484, 136, '01:40:43');
+INSERT INTO broadcasts VALUES (1253347658, 49073484, 56, '00:31:18');
+INSERT INTO broadcasts VALUES (1251269587, 49073484, 17, '00:07:54');
+INSERT INTO broadcasts VALUES (1249064043, 49073484, 111, '01:51:30');
+INSERT INTO broadcasts VALUES (1247154983, 49073484, 123, '02:37:25');
+INSERT INTO broadcasts VALUES (1246428728, 49073484, 120, '01:21:44');
+
+-- INSERTING INTO clips -- krismf 
+-- these clips are within the 30 day timeframe
+INSERT INTO clips VALUES ('PatientDirtyManateeVoteNay', 1267786471, 8, '00:00:25', 'KrisMF');
+INSERT INTO clips VALUES ('DiligentCalmDotterelNerfRedBlaster', 1267786471, 6, '00:00:25', 'KrisMF');
+INSERT INTO clips VALUES ('ElatedGiftedBeaverPMSTwin', 1267786471, 5, '00:00:29', 'eighteen00');
+INSERT INTO clips VALUES ('BadDepressedGarlicTBTacoLeft', 1281518341, 3, '00:00:29', 'SilverPsych');
+INSERT INTO clips VALUES ('MagnificentArtisticBillDeIlluminati', 1267786471, 3, '00:00:29', 'eighteen00');
+INSERT INTO clips VALUES ('TallShortWerewolfDxAbomb', NULL, 2, '00:00:29', 'KrisMF');
+INSERT INTO clips VALUES ('IronicVictoriousDuckYouDontSay', 1267786471, 2, '00:00:49', 'eighteen00');
+INSERT INTO clips VALUES ('RamshackleCreativeWoodcockBuddhaBar', 1289337440, 1, '00:00:27', 'eighteen00');
+INSERT INTO clips VALUES ('FriendlyCrypticWeaselGingerPower', 1282813535, 1, '00:00:29', 'SilverPsych');
+INSERT INTO clips VALUES ('ProductiveGrossShieldStinkyCheese', 1264530049, 1, '00:00:29', 'KrisMF');
+INSERT INTO clips VALUES ('PopularSassySangFutureMan', 1267786471, 2, '00:00:26', 'eighteen00');
+
+-- CREATED BY: 6AMRUN
+-- FEBRUARY 08, 2022
+
+-- INSERTING INTO broadcasts -- holykray
+INSERT INTO broadcasts VALUES (1291970074, 66682416, 48, '00:35:31');
+INSERT INTO broadcasts VALUES (1292010683, 66682416, 78, '01:08:30');
+
+-- INSERTING INTO clips -- holykray
+-- these clips are within the 30 day timeframe
+INSERT INTO clips VALUES ('SolidBetterBatteryGivePLZ', 1292010683, 3, '00:00:27', 'SilverPsych');
+INSERT INTO clips VALUES ('HotSmoggyBottlePeanutButterJellyTime', NULL, 3, '00:00:27', 'kinganguay916');
+INSERT INTO clips VALUES ('PoliteFreezingWormSmoocherZ', NULL, 2, '00:00:08', 'MugiwaraGabe');
+INSERT INTO clips VALUES ('CharmingInexpensiveGrouseGingerPower', NULL, 2, '00:00:29', 'MugiwaraGabe');
